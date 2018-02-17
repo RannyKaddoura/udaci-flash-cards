76f25178f4dc8650d1bbe260f2b9d794e1dc6af3
Frederik Trostorf <fredtrostorf@gmail.com>
2018-02-17 13:42:12 +0100

    feature: Add Navigation and statusbar

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import Navigation from './app/Navigation';
+import { Constants } from 'expo';
+import { blue } from './utils/colors';
+
+function FlashcardsStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  );
+}
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <FlashcardsStatusBar backgroundColor={blue} barStyle="light-content" />
         <Navigation />
       </View>
     );
@@ -16,5 +27,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff'
-  },
+  }
 });
