7ee90b1eafdb0563229ff865e78f462ba86a6180
Frederik Trostorf <fredtrostorf@gmail.com>
2018-02-17 13:23:09 +0100

    feat: add tab navigation

--- app/Navigation.js
+++ app/Navigation.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import DeckList from '../deck/DeckList';
+import AddDeck from '../deck/AddDeck';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import { gray, white } from '../utils/colors';
 
-export default class Navigation extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Navigation</Text>
-      </View>
-    );
+const Navigation = TabNavigator(
+  {
+    DeckList: {
+      screen: DeckList,
+      navigationOptions: {
+        tabBarLabel: 'Decks',
+        tabBarIcon: ({tintColor}) => <Ionicons name="ios-list-outline" size={30} color={white} />
+      }
+    },
+    AddDeck : {
+      screen: AddDeck,
+      navigationOptions: {
+        tabBarLabel: 'Add Deck',
+        tabBarIcon: ({tintColor}) => <FontAwesome name="plus-square" size={30} color={tintColor} />
+      }
+    }
+  },
+  {
+    navigationOptions: {
+      header: null
+    },
+    tabBarOptions: {
+      activeTintColor: white,
+      style: {
+        height: 56,
+        backgroundColor: gray,
+        shadowColor: 'rgba(0, 0, 0, 0.24)',
+        shadowOffset: {
+          width: 0,
+          height: 3
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1
+      },
+      labelStyle: {
+        fontSize: 14
+      }
+    }
   }
-}
+);
+
+export default Navigation;
