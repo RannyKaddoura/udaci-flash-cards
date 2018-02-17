a209a032c3c172dd545cd554f105584edc3804a2
Frederik Trostorf <fredtrostorf@gmail.com>
2018-02-17 12:58:00 +0100

    feature: react class markup

---
+++ app/Navigation.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class Navigation extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Navigation</Text>
+      </View>
+    );
+  }
+}
