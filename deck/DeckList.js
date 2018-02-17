e1cd838ae298403c19c00168d61c7a580477fff4
Frederik Trostorf <fredtrostorf@gmail.com>
2018-02-17 14:19:30 +0100

    feature: add dummy data

--- deck/DeckList.js
+++ deck/DeckList.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, AsyncStorage } from 'react-native'
+import { fetchDecks } from '../utils/api';
+import { DECKS_STORAGE_KEY } from '../utils/_decks'
 
 export default class DeckList extends Component {
+
+  state = {
+    decks: {}
+  }
+
+  componentDidMount(){
+    fetchDecks()
+      .then((decks) => {
+        this.setState({decks});
+      });
+  }
+
   render() {
+    console.log(this.state.decks)
     return (
       <View>
-        <Text>DeckList</Text>
+        <FlatList
+          data={this.state.decks}
+          renderItem={({item}) => {
+            return (<Text>{item.title}</Text>);
+          }}
+        />
       </View>
     );
   }
---
+++ utils/_decks.js
@@ -0,0 +1,41 @@
+import { AsyncStorage } from 'react-native';
+
+export const DECKS_STORAGE_KEY = 'flashcards:decks';
+
+function setDummyData() {
+  const decks = [
+    {
+      title: 'React',
+      questions: [
+        {
+          question: 'What is React?',
+          answer: 'A library for managing user interfaces'
+        },
+        {
+          question: 'Where do you make Ajax requests in React?',
+          answer: 'The componentDidMount lifecycle event'
+        }
+      ]
+    },
+    {
+      title: 'JavaScript',
+      questions: [
+        {
+          question: 'What is a closure?',
+          answer:
+            'The combination of a function and the lexical environment within which that function was declared.'
+        }
+      ]
+    }
+  ];
+  AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(decks));
+  return decks;
+}
+
+export function provideDecks(result) {
+  if (result === null) {
+    return setDummyData();
+  }
+
+  return JSON.parse(result);
+}
